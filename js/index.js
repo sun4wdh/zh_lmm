5ac0ae707f03cf3d441976aaf6f1074b6a70a988
zh <13667836737@163.com>
2018-11-24 18:49:15 +0800

    修改好了轮播图

--- js/index.js
+++ js/index.js
@@ -4,9 +4,10 @@ $(()=>{
   //轮播图初始化
   function lbtInit() {
     var mySwiper = new Swiper('.swiper-container', {
-      autoplay: 1000,
-      watchSlidesProgress : true,
-      watchSlidesVisibility : true,
+      autoplay: 2000,
+      autoplayDisableOnInteraction : false,
+      pagination : '.swiper-pagination',
+      paginationClickable :true,
       loop : true
     })
   }
